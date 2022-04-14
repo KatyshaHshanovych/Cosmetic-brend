c361b74adc26a92712514fd559bd9bf949c0fe1b
Kateryna Hshanovych <katrina.90994@gmail.com>
2022-04-14 16:15:13 +0300

    create a new block with cosmetic bottle

--- index.php
+++ index.php
@@ -2,8 +2,131 @@
 <html lang="ru">
 <head>
     <?php include 'head.php'; ?>
-    <title>Portfolio</title>
+    <title>Cosmetic</title>
 </head>
 <body>
-
+    <div class="header">
+        <div class="container">
+            <div class="header__blocks">
+                <div class="header__leftLogo">
+                    <img src="image/Logo_header.png" alt="logo" title="logo">
+                </div>
+                <div class="header__middle">
+                    <div class="header__aboutProducts">
+                        <a class="header__link" href="/">
+                            about
+                        </a>
+                        <a class="header__link" href="/">
+                            products
+                        </a>
+                    </div>
+                </div>
+                <div class="header__logIn">
+                    <a class="header__link" href="/">
+                        log in
+                    </a>
+                    <a class="header__link" href="/">
+                        cart
+                    </a>
+                    <a class="header__link" href="/">
+                        usd
+                    </a>
+                    <select>
+                        <option>
+                            ukr
+                        </option>
+                    </select>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="welcome-page">
+        <div class="welcome-page__personalCare">
+            <div class="welcome-page__title">
+                <h2 class="welcome-page__bigTitle">
+                    Hassle Free Personal Care
+                </h2>
+            </div>
+            <div class="welcome-page__subtitle">
+                Quality hair and skincare products for men and women delivered to your door
+            </div>
+            <div class="welcome-page__buttonBuy">
+                <a class="welcome-page__whiteBuy" href="/">
+                    buy now
+                </a>
+            </div>
+        </div>
+        <img class="welcome-page__landingImg" src="image/faceback.png" alt="face" title="face">
+    </div>
+    <div class="feedback-struggle">
+        <div class="feedback-struggle__feedback">
+            <div class="feedback-struggle__leftImage">
+                <img class="welcome-page__imgGirl" src="image/portrait.png" alt="girl" title="girl">
+            </div>
+            <div class="feedback-struggle__information">
+                <div class="feedback-struggle__textFeedback">
+                    <p class="feedback-struggle__girlsText">
+                        I don’t wear makeup very often, but when I do I go for a natural, no-makeup look. This skin tint and concealer gives exactly that. It helps even out my skin tone and looks very natural.
+                    </p>
+                    <div class="feedback-struggle__namePerson">
+                        <div class="feedback-struggle__minus">
+                            —
+                        </div>
+                        <div class="feedback-struggle__nameGirl">
+                            Scarlett Black
+                        </div>
+                    </div>
+                </div
+            </div>
+        </div>
+    </div>
+    <div class="gridInform">
+        <div class="container">
+            <div class="gridInform__nameTitle">
+                Do you struggle with
+            </div>
+            <div class="gridInform__infoGrid">
+                <div class="gridInform__blocks">
+                    <div class="gridInform__nameImg">
+                        <img src="image/care/bathroom.png" alt="img" title="img">
+                    </div>
+                    <div class="gridInform__nameBlock">
+                        Bathroom Clutter
+                    </div>
+                    <div class="gridInform__description">
+                        Too many products for everyone in your household
+                    </div>
+                </div>
+                <div class="gridInform__blocks">
+                    <div class="gridInform__nameImg">
+                        <img src="image/care/dry_skin.png" alt="img" title="img">
+                    </div>
+                    <div class="gridInform__nameBlock">
+                        Bathroom Clutter
+                    </div>
+                    <div class="gridInform__description">
+                        Too many products for everyone in your household
+                    </div>
+                </div>
+                <div class="gridInform__blocks">
+                    <div class="gridInform__nameImg">
+                        <img src="image/care/damaged.png" alt="img" title="img">
+                    </div>
+                    <div class="gridInform__nameBlock">
+                        Bathroom Clutter
+                    </div>
+                    <div class="gridInform__description">
+                        Too many products for everyone in your household
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="cosmetic-info">
+        <div class="container">
+            <div class="cosmetic-info__titleBig">
+                why united and free is the effortless choice
+            </div>
+        </div>
+    </div>
 </body>
